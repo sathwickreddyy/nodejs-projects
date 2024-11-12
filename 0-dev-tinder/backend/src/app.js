11b124f0b082394e8c0a538f816e681d9434822f
Sathwick reddy <sathwickreddyy@outlook.com>
2024-11-12 23:27:30 +0530

    CRUD operations

--- 0-dev-tinder/backend/src/app.js
+++ 0-dev-tinder/backend/src/app.js
@@ -7,6 +7,23 @@ app.use("/hello", (req, res) => {
     res.send("Hello Sathwick from server");
 })
 
+app.get("/user", (req, res) => {
+    res.send("Hello I am User Sathwick");
+});
+
+app.post("/user", (req, res) => {
+    res.send("Created User");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted User");
+});
+
+app.put("/user", (req, res) => {
+    res.send("Updated User");
+});
+
+
 app.use("/", (req, res)=>{
     res.send("Hello World")
 });
