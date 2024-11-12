3872868be3d9bef0df176f02404d15b193444489
Sathwick reddy <sathwickreddyy@outlook.com>
2024-11-12 22:44:30 +0530

    Order of Routes Is Very Important

--- 0-dev-tinder/backend/src/app.js
+++ 0-dev-tinder/backend/src/app.js
@@ -2,13 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res)=>{
-    res.send("Hello World")
-});
-
+// Order of writing the routes matter in express.js
 app.use("/hello", (req, res) => {
     res.send("Hello Sathwick from server");
 })
+
+app.use("/", (req, res)=>{
+    res.send("Hello World")
+});
 
 app.listen(3000, () => {
     console.log("Hey Sathwick - Server started on port 3000");
