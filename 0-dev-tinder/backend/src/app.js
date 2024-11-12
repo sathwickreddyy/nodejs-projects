2dc2add9cbe85e48e8daaada16aefc41dcd48110
Sathwick reddy <sathwickreddyy@outlook.com>
2024-11-12 22:18:29 +0530

    Hello from Server : RESPONSE

--- 0-dev-tinder/backend/src/app.js
+++ 0-dev-tinder/backend/src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res) => {
+    res.send("Hello Sathwick from server");
+})
+
 app.listen(3000, () => {
     console.log("Hey Sathwick - Server started on port 3000");
 });
