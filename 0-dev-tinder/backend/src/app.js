84da44fc0cf45fabead2b3cef2ed2c22ca169b7c
Sathwick reddy <sathwickreddyy@outlook.com>
2024-11-13 14:59:15 +0530

    next() in route handler - chaining of route handlers

--- 0-dev-tinder/backend/src/app.js
+++ 0-dev-tinder/backend/src/app.js
@@ -2,32 +2,16 @@ const express = require("express");
 
 const app = express();
 
-// Order of writing the routes matter in express.js
-app.use("/hello", (req, res) => {
-    res.send("Hello Sathwick from server");
-})
+app.use("/user", (req, res, next) => {
+        // Route handler
+        console.log("In first handler");
+        next();
+    },
+    (req, res) => {
+        console.log("handled in the second handler");
+        res.send("Hello World from second");
+    })
 
-app.get("/user", (req, res) => {
-    res.send("Hello I am User Sathwick");
-});
-
-app.post("/user", (req, res) => {
-    res.send("Created User");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("Deleted User");
-});
-
-app.put("/user", (req, res) => {
-    res.send("Updated User");
-});
-
-
-app.use("/", (req, res)=>{
-    res.send("Hello World")
-});
-
-app.listen(3000, () => {
-    console.log("Hey Sathwick - Server started on port 3000");
+app.listen(7777, () => {
+    console.log("Hey Sathwick - Server started on port 7777");
 });
