c0650e41e511c23c1058c9d965c28161b5aebb8c
Sathwick reddy <sathwickreddyy@outlook.com>
2024-11-17 05:22:37 +0530

    Adding Expiry for token & cookies

--- 0-dev-tinder/backend/src/app.js
+++ 0-dev-tinder/backend/src/app.js
@@ -57,8 +57,12 @@ app.post("/login",  async(req, res) => {
             throw new Error("Invalid Credentials");
         }
 
-        const token = await jwt.sign({_id: user._id}, "Secret@Tinder$790");
-        res.cookie("token", token);
+        const token = await jwt.sign({_id: user._id}, "Secret@Tinder$790", {
+            expiresIn: "1d"
+        });
+        res.cookie("token", token, {
+            expires: new Date(Date.now() + 86400000),
+        });
         res.send("Login successful");
     }
     catch (err) {
@@ -75,69 +79,15 @@ app.get("/profile", userAuth, async (req, res) => {
         console.error("Error while getting profile", err);
         res.status(400).send({ error: "Error while getting profile details: "+ err.message +". Please login again" });
     }
-})
-
-app.get("/user", async (req, res)=>{
-    const firstName = req.body.firstName;
-    try
-    {
-        const users = await User.find({ firstName: firstName }).catch((err) => {
-            console.error("Error while finding user", err);
-        });
-        if(users.length === 0){
-            res.status(404).send({ error: "User not found" });
-        }
-        res.send(users);
-    }
-    catch (err) {
-        console.error("Error while finding user", err);
-        res.status(500).send({ error: "Error while finding user" });
-    }
-
-})
-
-app.get("/feed", userAuth, async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    }
-    catch (err) {
-        console.error("Error while finding users", err);
-        res.status(500).send({ error: "Error while finding users" });
-    }
-})
-
-app.delete("/user", userAuth ,async (req, res) => {
-    try {
-        const userId = req.body.userId;
-        const response = await User.findByIdAndDelete({"_id": userId});
-        res.send(response);
-    }
-    catch (err) {
-        console.error("Error while deleting user", err);
-        res.status(500).send({ error: "Error while deleting user" });
-    }
 });
 
-app.patch("/user/:userId", userAuth,async (req, res) => {
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
     try {
-
-        const ALLOWED_UPDATES = ["password", "age", "gender", "skills", "about"];
-        const userId = req.params.userId;
-        const user = req.body;
-
-        const isAllowed = Object.keys(user).every((k) => ALLOWED_UPDATES.includes(k));
-        if(!isAllowed) {
-            throw new Error("Update not allowed."+ " Allowed updates are " + ALLOWED_UPDATES);
-        }
-        const response = await User.findByIdAndUpdate({"_id": userId}, user, {
-            returnDocument: "after",
-            runValidators: true
-        })
-        res.send(response);
+        const user = req._user;
+        res.send(user.firstName +" Sending Connection request to");
     }
     catch (err) {
-        console.error("Error while updating user", err.message);
+        console.error("Error while sending connection request", err);
         res.status(400).send({ error: err.message });
     }
 })
