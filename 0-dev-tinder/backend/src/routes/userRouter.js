aa732c2d3d3355bd925be355fd1ab3b7a1d5713c
Sathwick reddy <sathwickreddyy@outlook.com>
2024-11-24 17:59:22 +0530

    Adding get /user/connections logic

--- 0-dev-tinder/backend/src/routes/userRouter.js
+++ 0-dev-tinder/backend/src/routes/userRouter.js
@@ -13,7 +13,7 @@ router.get("/user/requests/received", userAuth, async (req, res) => {
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: "interested"
-        }).populate("fromUserId", ["firstName", "lastName"]); // With the help of references we can select the required fields of the other collection.
+        }).populate("fromUserId", ["firstName", "lastName", "photoUrl"]); // With the help of references we can select the required fields of the other collection.
 
         res.status(200).json({
             "message": "Connection requests fetched successfully",
@@ -25,41 +25,47 @@ router.get("/user/requests/received", userAuth, async (req, res) => {
     }
 })
 
-
-/**
-Output looks like below:
-
-    {
-        "message": "Connection requests fetched successfully",
-        "data": [
-            {
-                "_id": "6739cd63a392acaa668fd7e5",
-                "fromUserId": {
-                    "_id": "67393eda6aa0129fb3678a61",
-                    "firstName": "Sunny",
-                    "lastName": "Reddy"
+router.get("/user/connections", userAuth, async (req, res) => {
+    try{
+        const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl"];
+        const loggedInUser = req._user;
+        /**
+         * Say I am user A
+         *
+         * A -> B Connection Request Accepted
+         * C -> A Connection Request Accepted
+         *
+         * Now i should have both B and C in my connections
+         */
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {
+                    fromUserId: loggedInUser._id,
+                    status: "accepted"
                 },
-                "toUserId": "673748d8733444aef05cef97",
-                "status": "interested",
-                "createdAt": "2024-11-17T11:02:59.530Z",
-                "updatedAt": "2024-11-17T11:02:59.530Z",
-                "__v": 0
-            },
-            {
-                "_id": "673f518daff0894224b2907f",
-                "fromUserId": {
-                    "_id": "673926ec7d949ff27d7565e5",
-                    "firstName": "Bunny",
-                    "lastName": "Reddy"
-                },
-                "toUserId": "673748d8733444aef05cef97",
-                "status": "interested",
-                "createdAt": "2024-11-21T15:28:13.978Z",
-                "updatedAt": "2024-11-21T15:28:13.978Z",
-                "__v": 0
+                {
+                    toUserId: loggedInUser._id,
+                    status: "accepted"
+                }
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA).populate("toUserId", USER_SAFE_DATA);
+
+        const data = connectionRequests.map((connectionRequest) => {
+            if(connectionRequest.fromUserId.toString() === loggedInUser._id.toString()) {
+                return connectionRequest.toUserId;
             }
-        ]
+            return connectionRequest.fromUserId;
+        });
+
+        res.status(200).json({
+            "message": "Connection requests fetched successfully",
+            "data": data
+        });
     }
-*/
+    catch (err) {
+        console.error("Error while getting connection requests", err);
+        res.status(400).send({error: err.message});
+    }
+})
 
 module.exports = router;
