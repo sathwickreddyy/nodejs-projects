977b730707445082db88933a091c36c7919f2b1d
Sathwick reddy <sathwickreddyy@outlook.com>
2024-11-24 19:21:00 +0530

    Adding Feed Logic to Backend

--- 0-dev-tinder/backend/src/routes/userRouter.js
+++ 0-dev-tinder/backend/src/routes/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {userAuth} = require("../middlewares/auth");
 const router = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 // Get all pending connection requests for the loggedIn user
 router.get("/user/requests/received", userAuth, async (req, res) => {
@@ -27,7 +28,7 @@ router.get("/user/requests/received", userAuth, async (req, res) => {
 
 router.get("/user/connections", userAuth, async (req, res) => {
     try{
-        const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl"];
+        const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "age", "skills", "gender"];
         const loggedInUser = req._user;
         /**
          * Say I am user A
@@ -66,6 +67,65 @@ router.get("/user/connections", userAuth, async (req, res) => {
         console.error("Error while getting connection requests", err);
         res.status(400).send({error: err.message});
     }
-})
+});
+
+router.get("/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req._user;
+        const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "age", "skills", "gender"];
+
+        /**
+         * Say A is loggedIn User
+         *
+         * Feed shouldn't have users whom A is already connected with & whom he ignored & himself & Other users who are not connected with A & if he already sent connection
+         * request sent.
+         *
+         * Example: There are 10 existing users say names Sunny, Bunny, Rina, Jenny, Sathwick, Rithwick, Bharath, Trump, Elon, Modi
+         * A new user Amit came in
+         *      * Amit should see all the users except himself.
+         *      * Amit should not see the users who are already connected with Amit.
+         *      * Amit should not see the users who are already ignored by Amit.
+         *      * Amit should not see the users who are already sent connection request to Amit.
+         *      * Amit should not see the users who are already sent connection request from Amit.
+         */
+
+        const hideUsersFromFeed = new Set();
+
+        // find all connections for all the sent and received connection requests
+        const connectionRequests = await ConnectionRequest.find({
+            "$or": [
+                    {
+                        fromUserId: loggedInUser._id
+                    },
+                    {
+                        toUserId: loggedInUser._id
+                    }
+            ]
+        }).select("fromUserId toUserId status");
+
+        connectionRequests.forEach((connectionRequest) => {
+            hideUsersFromFeed.add(connectionRequest.fromUserId.toString());
+            hideUsersFromFeed.add(connectionRequest.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select(USER_SAFE_DATA);
+
+        res.status(200).json({
+            "message": "Feed fetched successfully",
+            "data": users
+        });
+
+    }
+    catch (err) {
+        console.error("Error while getting feed", err);
+        res.status(400).send({error: err.message});
+    }
+});
+
 
 module.exports = router;
