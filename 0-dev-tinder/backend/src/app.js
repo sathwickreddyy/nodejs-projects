1610ddd4d3f00c625cb3b5a0f723463dc9cab2cc
Sathwick reddy <sathwickreddyy@outlook.com>
2024-11-26 16:43:03 +0530

    Enable Cross Origin Requests

--- 0-dev-tinder/backend/src/app.js
+++ 0-dev-tinder/backend/src/app.js
@@ -5,9 +5,11 @@ const authRouter = require(("./routes/authRouter"));
 const profileRouter = require(("./routes/profileRouter"));
 const requestRouter = require(("./routes/requestRouter"));
 const userRouter = require("./routes/userRouter")
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", authRouter);
